0b0067ffe4e81a0f4bd70d99eea658ef3f4b696d
Sergio Flesca <sergio.flesca@unical.it>
2022-03-02 14:28:47 +0100

    aggiunti esempi lezione

--- semplici/simple.py
+++ semplici/simple.py
@@ -5,30 +5,30 @@ def sommamatrici(a, b):
     c = np.zeros(a.shape)
     for i in range(c.shape[0]):
         for j in range(c.shape[1]):
-            c[i][j] = a[i][j]+b[i][j]
+            c[i][j] = a[i][j] + b[i][j]
     return c
 
 
-def moltiplicazionematrici(a,b):
+def moltiplicazionematrici(a, b):
     c = np.zeros((a.shape[0], b.shape[1]))
     for i in range(len(c)):
         for j in range(len(c[i])):
             for k in range(len(a[i])):
-                c[i][j] += a[i][k]*b[k][j]
+                c[i][j] += a[i][k] * b[k][j]
     return c
 
 
 def fatt(n):
-    if n>1:
-        return fatt(n-1)*n
+    if n > 1:
+        return fatt(n - 1) * n
     else:
         return 1
 
 
 def fatit(n):
     ft = 1
-    for i in range(1,n+1):
-        ft *=i
+    for i in range(1, n + 1):
+        ft *= i
     return ft
 
 
@@ -37,3 +37,17 @@ def search(vett, x):
         if v == x:
             return True
     return False
+
+
+def somma(vett):
+    x = 0
+    for v in vett:
+        x += v
+    return x
+
+
+def sommainefficiente(vett):
+    x = [0 for i in range(len(vett)+1)]
+    for i in range(len(vett)):
+        x[i+1] = vett[i] + x[i]
+    return x[len(vett)]
--- semplici/test.py
+++ semplici/test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import time
 
-from semplici.simple import moltiplicazionematrici, sommamatrici, fatt, fatit, search
+from semplici.simple import moltiplicazionematrici, sommamatrici, fatt, fatit, search, somma, sommainefficiente
 
 # m1 = np.array([[0, 1], [2, 3]])
 # m2 = np.array([[0, 2, 3], [1, 4, 5]])
@@ -17,8 +17,25 @@ from semplici.simple import moltiplicazionematrici, sommamatrici, fatt, fatit, s
 # print(trovato)
 
 
-v1 = np.random.randint(0, 10000000, 10000000)
+v1 = np.random.randint(0, 10000000, 10000)
+x = -1
 ts = time.time()
-trovato = search(v1, 14)
+trovato = search(v1, x)
 te = time.time() - ts
-print("Tempo =" + str(te))
+if trovato:
+    print(str(x)+" è presente")
+else:
+    print(str(x) + " non è presente")
+print("Tempo ricerca = " + str(te)+" secondi")
+
+
+# v1 = np.random.randint(0, 4, 100000000)
+# ts = time.time()
+# s = somma(v1)
+# te = time.time() - ts
+# print(str(s)+"è la somma")
+# print("Tempo ricerca = " + str(te)+" secondi")
+
+v1 = [1, 2,3, 6, 28, 2]
+# print(somma(v1))
+print(sommainefficiente(v1))

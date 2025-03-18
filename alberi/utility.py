2e2347f7ace7e73b75f763b05b322c323ee61115
sflesca <sergio.flesca@unical.it>
2025-03-18 12:48:38 +0100

    quicksort.py

--- alberi/utility.py
+++ alberi/utility.py
@@ -1,4 +1,5 @@
 import sys
+from platform import android_ver
 
 from alberi.alberibinari import AlberoBin
 
@@ -33,3 +34,50 @@ def verifica1(a: AlberoBin)->bool:
     if a is None or (a.sin is None and a.des is None):
         return True
     return massimo(a.sin)<massimo(a.des) and verifica1(a.sin) and verifica1(a.des)
+
+
+def almenounafoglia(a: AlberoBin, val)->bool:
+    if a is None:
+        return False
+    if not a.sin and not a.des:
+        return a.val == val
+    return almenounafoglia(a.sin, val) or almenounafoglia(a.des, val)
+
+
+def tuttiminori(a:AlberoBin, val)->bool:
+    if a is None:
+        return True
+    return a.val<val and tuttiminori(a.sin, val) and tuttiminori(a.des, val)
+
+
+def tuttimaggiori(a:AlberoBin, val)->bool:
+    if a is None:
+        return True
+    return a.val>=val and tuttimaggiori(a.sin, val) and tuttimaggiori(a.des, val)
+
+
+def diricerca(a: AlberoBin)->bool:
+    if a is None:
+        return True
+    return (tuttiminori(a.sin, a.val) and tuttimaggiori(a.des, a.val)
+            and diricerca(a.sin) and diricerca(a.des))
+
+
+
+
+
+
+
+
+
+
+def diricercasbagliato(a: AlberoBin)->bool:
+    if a is None:
+        return True
+    if (a.sin is not None):
+        if a.sin.val >= val:
+            return False
+    if (a.des is not None):
+        if a.des.val >= val:
+            return False
+    return diricercasbagliato(a.sin) and diricercasbagliato(a.des)
